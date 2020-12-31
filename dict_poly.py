fd9c2b726e6a43469bcfe0287dd137e0f6804ce5
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2020-12-31 12:28:49 +0300

    31 12 20

--- dict_poly.py
+++ dict_poly.py
@@ -5,7 +5,9 @@ def insert_factorial():
     сумма цивр факториал 100 math
     :return:
     """
+    start = time.time()
     res = math.factorial(100)
+    print(time.time() - start)
     return str(sum(int(x) for x in str(res)))
 
 def manual_fuctorial():
@@ -13,18 +15,18 @@ def manual_fuctorial():
         сумма цивр факториал 100 вручную
         :return:
         """
+    start = time.time()
     i = 1
     res = 1
     while i <= 100:
         res = res * i
         i += 1
+    print(time.time() - start)
     return str(sum(int(x) for x in str(res)))
 
 
-start=time.time()
-print(insert_factorial())
-print(time.time()-start)
 
-start=time.time()
+print(insert_factorial())
+
 print(manual_fuctorial())
-print(time.time()-start)
+
