34328912c1bc90dd522b3343d28018b6a96ecb4f
vladimir <grun_gespenst@tut.by>
2021-01-04 12:49:04 +0300

    war

--- army.py
+++ army.py
@@ -1,18 +1,31 @@
 from random import random as ran
 
 class Warrior:
-    def __init__(self, name, health, demage, side):
+    def __init__(self, name, health, damage):
         self.name = name
         self.health = health
-        self.demage = demage
-        self.side = side
+        self.damage = damage
+
 
     def get_info(self):
-        return self.side, self.name, self.health
+        return  self.name, self.health
 
-Dart_Waider = Warrior('Waider', 120, 12, 'Dark-side')
-Dart_Sidius = Warrior('Sidius', 100, 15, 'Dark-side')
-Master_Ioda = Warrior('Ioda', 100, 15, 'Light-side')
-Obi_Van = Warrior('Obi_Van', 150, 15, 'Light-side')
+class Army_dark_side(Warrior):
+    def __init__(self, name, health, damage, side):
+        super().__init__(name, health, damage)
+        self.side = side
 
-while
+class Army_light_side(Warrior):
+    def __init__(self, name, health, damage, side):
+        super().__init__(name, health, damage)
+        self.side = side
+
+
+Dart_Waider = Army_dark_side('Waider', 120, 12, 'Dark')
+Dart_Sidius = Army_dark_side('Sidius', 100, 15, 'Dark')
+Master_Ioda = Army_light_side('Ioda', 100, 15, 'Light')
+Obi_Van = Army_light_side('Obi_Van', 150, 15, 'Light')
+Bobo_Fet = Warrior('Bobo', 80, 7)
+
+print(Master_Ioda.side)
+print(Bobo_Fet.side)
