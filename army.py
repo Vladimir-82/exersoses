27b858829af403b8a64d803bc36a091c126de017
vladimir <grun_gespenst@tut.by>
2021-01-05 21:37:14 +0300

    war

--- army.py
+++ army.py
@@ -1,4 +1,4 @@
-from random import random as ran
+from random import randint as ran
 
 class Warrior:
     def __init__(self, name, health, damage):
@@ -25,25 +25,32 @@ class Neitral(Warrior):
         super().__init__(name, health, damage)
         self.side = side
 
-
-Dart_Waider = Army_dark_side('Waider', 120, 12, 'Dark')
-Dart_Sidius = Army_dark_side('Sidius', 100, 15, 'Dark')
-Master_Ioda = Army_light_side('Ioda', 100, 15, 'Light')
-Obi_Van = Army_light_side('Obi_Van', 150, 15, 'Light')
-Bobo_Fet = Neitral('Bobo', 50, 5, 'Neitral')
+def choce_batle(Army):
+    choice = ran(0, len(Army)-1)
+    return choice
 
 Light_warriors_list = []
 Dark_warriors_list = []
 Neitral_warriors_list = []
-for j in Dart_Waider.name, Dart_Sidius.name, Master_Ioda.name, Obi_Van.name, Bobo_Fet.name:
-    for i in Dart_Waider.side, Dart_Sidius.side, Master_Ioda.side, Obi_Van.side, Bobo_Fet.side:
-        if i == 'Dark':
-            Dark_warriors_list.append(j)
-        elif i == 'Light':
-            Light_warriors_list.append(j)
-        else:
-            Neitral_warriors_list.append(j)
+
+
+Dart_Waider = Army_dark_side('Waider', 120, 12, 'Dark')
+Dark_warriors_list.append(Dart_Waider.name)
+Dart_Sidius = Army_dark_side('Sidius', 100, 15, 'Dark')
+Dark_warriors_list.append(Dart_Sidius.name)
+Master_Ioda = Army_light_side('Ioda', 100, 15, 'Light')
+Light_warriors_list.append(Master_Ioda.name)
+Obi_Van = Army_light_side('Obi_Van', 150, 15, 'Light')
+Light_warriors_list.append(Obi_Van.name)
+Bobo_Fet = Neitral('Bobo', 50, 5, 'Neitral')
+Neitral_warriors_list.append(Bobo_Fet.name)
 
 print(Dark_warriors_list)
 print(Light_warriors_list)
 print(Neitral_warriors_list)
+
+while len(Dark_warriors_list)>0 and len(Light_warriors_list)>0:
+     choce_dark = choce_batle(Dark_warriors_list)
+     choce_light = choce_batle(Light_warriors_list)
+     print(choce_dark)
+     #print(Dark_warriors_list[choce_dark], 'fight us', Light_warriors_list[choce_light])
