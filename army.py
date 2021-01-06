9dcf5811b73555ab0bb4236e3677338d01fea72e
vladimir <grun_gespenst@tut.by>
2021-01-06 17:34:02 +0300

    war

--- army.py
+++ army.py
@@ -1,5 +1,5 @@
 from random import randint as ran
-
+from time import sleep as sleep
 class Warrior:
     def __init__(self, name, health, damage):
         self.name = name
@@ -29,6 +29,26 @@ def choce_batle(Army):
     choice = ran(0, len(Army)-1)
     return choice
 
+def battle(unit1, unit2):
+    while unit1.health > 0 and unit2.health > 0:
+        kick = ran(1, 2)
+        if kick==1:
+            print(unit1.name, 'наносит удар')
+            sleep(1)
+            unit2.health = unit2.health - unit1.damage
+            print(unit2.get_info(), 'урон=', unit1.damage)
+        elif kick == 2:
+            print(unit2.name, 'наносит удар')
+            sleep(1)
+            unit1.health = unit1.health - unit2.damage
+            print(unit1.get_info(), 'урон=', unit2.damage)
+    if unit1.health <= 0:
+        print(unit1.name, 'убит')
+    else:
+        print(unit2.name, 'убит')
+
+    #print(unit1.name, 'убит' if unit1.health <= 0 else unit2.name, 'убит')
+
 Light_warriors_list = []
 Dark_warriors_list = []
 Neitral_warriors_list = []
@@ -45,12 +65,10 @@ Light_warriors_list.append(Obi_Van.name)
 Bobo_Fet = Neitral('Bobo', 50, 5, 'Neitral')
 Neitral_warriors_list.append(Bobo_Fet.name)
 
-print(Dark_warriors_list)
-print(Light_warriors_list)
-print(Neitral_warriors_list)
+battle(Dart_Waider, Obi_Van)
 
-while len(Dark_warriors_list)>0 and len(Light_warriors_list)>0:
-     choce_dark = choce_batle(Dark_warriors_list)
-     choce_light = choce_batle(Light_warriors_list)
-     print(choce_dark)
+#while len(Dark_warriors_list) > 0 and len(Light_warriors_list) > 0:
+     #choce_dark = choce_batle(Dark_warriors_list)
+     #choce_light = choce_batle(Light_warriors_list)
      #print(Dark_warriors_list[choce_dark], 'fight us', Light_warriors_list[choce_light])
+
